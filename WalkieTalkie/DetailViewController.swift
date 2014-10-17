035590454347aa939ecd546972c95eabc474e83e
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2014-10-17 20:44:43 +0300

    Empty Xcode project

---
+++ WalkieTalkie/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  WalkieTalkie
+//
+//  Created by Victor Ilyukevich on 10/17/14.
+//  Copyright (c) 2014 CocoaHeadsBY. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  var detailItem: AnyObject? {
+    didSet {
+        // Update the view.
+        self.configureView()
+    }
+  }
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail: AnyObject = self.detailItem {
+        if let label = self.detailDescriptionLabel {
+            label.text = detail.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.configureView()
+  }
+}
+
