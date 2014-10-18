6dacbdd6a4c7e870d462dcb29adf4ab7d39999f6
Aliaksandr Andrashuk <limejelly@yandex.ru>
2014-10-18 12:57:36 +0300

    Base for converter #2

--- WalkieTalkie/AACAudioConverter.swift
+++ WalkieTalkie/AACAudioConverter.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import AVFoundation
+import CoreAudio
+import AudioToolbox
+
+class AACAudioEncoder {
+    
+    var audioConverter : Unmanaged<AudioConverter>?;
+    
+    init (sourceFormat : UnsafePointer<AudioStreamBasicDescription>) {
+
+    }
+    
+    func doConvert() {
+        println("will encode to aac")
+    }
+}
+
+class AACAudioDecoder {
+    func doConvert() {
+        println("will decode from aac")
+    }
+}
