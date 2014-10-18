e7b4e294b9736a84faae0d15cad581862a38a94c
Aliaksandr Andrashuk <limejelly@yandex.ru>
2014-10-18 12:57:11 +0300

    Base for converter

---
+++ WalkieTalkie/AACAudioConverter.swift
@@ -0,0 +1,9 @@
+//
+//  AACAudioConverter.swift
+//  WalkieTalkie
+//
+//  Created by Aliksandr Andrashuk on 18.10.14.
+//  Copyright (c) 2014 CocoaHeadsBY. All rights reserved.
+//
+
+import Foundation
